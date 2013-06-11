7feb403bd886873acbb0ded95cae507d2980c113
DMM <dmm@ubuntu.(none)>
2013-06-11 14:12:30 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'f6bab51c48b1a8faeb1a495cdeb6282d3ac7ea2cb3008348deffaee0ba09e056b6f2ff2c402f156609005524aa4372bdd542fccd81e4226d617070788f31af81'
